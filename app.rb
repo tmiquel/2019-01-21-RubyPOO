6a0cc7ff55822f05e714003efd10899930519fd8
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-01-21 18:20:13 +0100

    App.rb updated

--- app.rb
+++ app.rb
@@ -11,3 +11,15 @@ require_relative 'lib/event'
 
 # Open bar pour tester ton application. Tous les fichiers importants sont chargés
 # Tu peux faire User.new, Event.new, binding.pry, User.all, etc etc
+
+
+
+
+User.new("julie@julie.com")
+User.new("jean@jean.com")
+
+julie = User.find_by_email("julie@julie.com")
+jean = User.find_by_email("jean@jean.com")
+
+e = Event.new("2019-01-13 09:00", 10, "standup quotidien", [julie, jean])
+puts "Voici l'email du premier attendee de l'événement : #{e.attendees.first.email}"
